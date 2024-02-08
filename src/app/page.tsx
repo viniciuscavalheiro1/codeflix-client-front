44e2f155c947fa9da54ce81c08b33165640bca59
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2024-02-08 01:40:46 -0300

    chore: style pricipal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      
-    </main>
+  return(
+    <div className='relative h-full bg-black lg:h-[140vh]'>
+      <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'></main>
+    </div>
   );
 }
